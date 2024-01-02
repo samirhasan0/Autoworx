7c5ee64ea157d270e7977a504f703c7138f7616b
KR Shanto <krshanto2005@gmail.com>
2024-01-02 21:51:12 +0600

    Show tasks in Tasks section in right-hand-side calendar

--- resources/js/Components/Task/users/Body.tsx
+++ resources/js/Components/Task/users/Body.tsx
@@ -1,8 +1,10 @@
 import { useCalendarUserTypeStore } from "@/stores/calendarUserType";
 import Users from "./Users";
+import Tasks from "./Tasks";
 
 export default function Body() {
   const { calendarUserType } = useCalendarUserTypeStore();
 
   if (calendarUserType === "USERS") return <Users />;
+  if (calendarUserType === "TASKS") return <Tasks />;
 }
---
+++ resources/js/Components/Task/users/Tasks.tsx
@@ -0,0 +1,34 @@
+import { TASK_COLOR } from "@/lib/const";
+import { usePopupStore } from "@/stores/popup";
+import { useTaskStore } from "@/stores/tasks";
+import { FaPlus } from "react-icons/fa";
+
+export default function Tasks() {
+  const { tasks } = useTaskStore();
+  const { open } = usePopupStore();
+
+  return (
+    <div className="bg-white calender-shadow h-[660px] mt-5 rounded-[12px] p-3 overflow-y-auto">
+      <h2 className="text-black text-[19px]">Task List</h2>
+
+      <div className="flex flex-wrap mt-3 gap-3">
+        {tasks.map((task) => (
+          <div
+            key={task.id}
+            className="rounded-full text-white text-[17px] py-2 px-4"
+            style={{ backgroundColor: TASK_COLOR[task.type] }}
+          >
+            {task.title}
+          </div>
+        ))}
+
+        <button
+          className="rounded-full text-[17px] text-white py-2 px-14 bg-[#797979]"
+          onClick={() => open("ADD_TASK")}
+        >
+          <FaPlus />
+        </button>
+      </div>
+    </div>
+  );
+}
