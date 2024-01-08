15b5e63afb7522277447d7dedb2ae05080b0f3ce
KR Shanto <krshanto2005@gmail.com>
2024-01-08 12:34:46 +0600

    Fix the dates in `Month` component.

--- resources/js/Components/Task/calendar/Month.tsx
+++ resources/js/Components/Task/calendar/Month.tsx
@@ -3,13 +3,14 @@ import { TASK_COLOR } from "@/lib/const";
 import { usePopupStore } from "@/stores/popup";
 import { useTaskStore } from "@/stores/tasks";
 import { TaskType } from "@/types/task";
+import moment from "moment";
 
 export default function Month() {
   const { open } = usePopupStore();
   const { tasks } = useTaskStore();
 
   // Initialize an array to hold the dates
-  const dates: [number, TaskType[] | undefined][] = [];
+  const dates: [Date, TaskType[] | undefined][] = [];
   // Define the total number of dates to be displayed
   const totalDates = 35;
   // Get the current date
@@ -26,14 +27,14 @@ export default function Month() {
     const date = new Date(today.getFullYear(), today.getMonth(), dateNumber);
     const tasks = getTasks(date);
     // Add the date to the dates array
-    dates.push([date.getDate(), tasks]);
+    dates.push([date, tasks]);
   }
 
   // Reverse the dates array to get the dates in ascending order
   dates.reverse();
 
   // Add today's date to the dates array
-  dates.push([today.getDate(), getTasks(today)]);
+  dates.push([today, getTasks(today)]);
 
   // Calculate the number of dates to be generated after today
   let rest = totalDates - todayIndex - 1;
@@ -45,12 +46,17 @@ export default function Month() {
     // Create a new date object
     const date = new Date(today.getFullYear(), today.getMonth(), dateNumber);
     // Add the date to the dates array
-    dates.push([date.getDate(), getTasks(date)]);
+    dates.push([date, getTasks(date)]);
   }
 
   function getTasks(date: Date) {
     return tasks.filter((task) => {
-      return new Date(task.date).getDate() === date.getDate();
+      // return new Date(task.date).getDate() === date.getDate();
+      return (
+        new Date(task.date).getFullYear() === date.getFullYear() &&
+        new Date(task.date).getMonth() === date.getMonth() &&
+        new Date(task.date).getDate() === date.getDate()
+      );
     });
   }
 
@@ -85,26 +91,17 @@ export default function Month() {
               className={cn(
                 "border-b border-r border-[#797979] flex flex-col items-end p-2 w-[160px] h-[115.5px] text-[23px] font-bold gap-2",
                 // check if the cell is today
-                today.getDate() === cell[0]
-                  ? "text-[#6571FF]"
-                  : "text-[#797979]"
+                today === cell[0] ? "text-[#6571FF]" : "text-[#797979]"
               )}
               onClick={() => {
                 open("ADD_TASK", {
-                  // TODO: Fix the date format
-                  // date: `${today.getFullYear()}-${today.getMonth() + 1}-${
-                  //   // Add a zero to the date if it is less than 10
-                  //   cell[0] < 10 ? "0" + cell[0] : cell[0]
-                  // }`,
-                  //*********************//
-                  // date: moment(
-                  //   new Date(today.getFullYear(), today.getMonth(), cell[0])
-                  // ).format("YYYY-MM-DD"),
+                  date: moment(cell[0]).format("YYYY-MM-DD"),
                 });
               }}
             >
-              {cell[0]}
-              <div className="flex flex-row gap-5">
+              {cell[0].getDate()}
+
+              <div className="flex gap-4 justify-end flex-wrap">
                 {cell[1]?.map((task: TaskType, i: number) => (
                   <div
                     key={i}
