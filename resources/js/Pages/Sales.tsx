0b4af220cdf2e925d8f8fcf0bcc7d3f684ddd15f
KR Shanto <krshanto2005@gmail.com>
2024-03-29 02:23:15 +0600

    Create `/sales` page

--- resources/js/Pages/Sales.tsx
+++ resources/js/Pages/Sales.tsx
@@ -1,5 +1,227 @@
 import Title from "@/Components/Title";
+import { useEffect, useState } from "react";
+
+const hotLeads = [
+  {
+    name: "",
+    email: "",
+    phone: "",
+  },
+  {
+    name: "",
+    email: "",
+    phone: "",
+  },
+  {
+    name: "",
+    email: "",
+    phone: "",
+  },
+  {
+    name: "",
+    email: "",
+    phone: "",
+  },
+  {
+    name: "",
+    email: "",
+    phone: "",
+  },
+];
+
+const onGoing = [
+  {
+    name: "",
+    email: "",
+    phone: "",
+  },
+  {
+    name: "",
+    email: "",
+    phone: "",
+  },
+  {
+    name: "",
+    email: "",
+    phone: "",
+  },
+  {
+    name: "",
+    email: "",
+    phone: "",
+  },
+  {
+    name: "",
+    email: "",
+    phone: "",
+  },
+];
+
+const intent = [
+  {
+    name: "",
+    email: "",
+    phone: "",
+  },
+  {
+    name: "",
+    email: "",
+    phone: "",
+  },
+  {
+    name: "",
+    email: "",
+    phone: "",
+  },
+  {
+    name: "",
+    email: "",
+    phone: "",
+  },
+  {
+    name: "",
+    email: "",
+    phone: "",
+  },
+];
+
+const purchased = [
+  {
+    name: "",
+    email: "",
+    phone: "",
+  },
+  {
+    name: "",
+    email: "",
+    phone: "",
+  },
+  {
+    name: "",
+    email: "",
+    phone: "",
+  },
+  {
+    name: "",
+    email: "",
+    phone: "",
+  },
+  {
+    name: "",
+    email: "",
+    phone: "",
+  },
+];
 
 export default function Sales() {
-  return <Title>Sales Funnel Management</Title>;
+  const [newLeads, setNewLeads] = useState<
+    { name: string; email: string; phone: number }[]
+  >([]);
+
+  // function to fetch google sheet data
+  async function fetchSheet() {
+    const SHEET_ID = "1rMyMol-aqx805TRNd9-_OkDdraPUCgzd1rOA_UT0Oas";
+    const SHEET_NAME = "autoworx test";
+    const SHEET_RANGE = "A2:Z100";
+
+    const url = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tq?sheet=${SHEET_NAME}&range=${SHEET_RANGE}`;
+
+    const res = await fetch(url);
+    const text = await res.text();
+    const json = JSON.parse(text.substr(47).slice(0, -2));
+    const rows = json.table.rows;
+
+    const leads = rows
+      .map((row: any) => {
+        return {
+          name: row.c[0].v,
+          email: row.c[1].v,
+          phone: row.c[2].v,
+        };
+      })
+      .reverse();
+
+    setNewLeads(leads);
+  }
+
+  // fetch data from google sheet
+  useEffect(() => {
+    // Call fetchSheet immediately after component mounts
+    fetchSheet();
+
+    // Set up interval to fetch data every n seconds
+    const intervalId = setInterval(fetchSheet, 5000);
+
+    // Clean up interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const data = [
+    {
+      title: "Hot Leads",
+      leads: hotLeads,
+    },
+    {
+      title: "New Leads",
+      leads: newLeads,
+    },
+    {
+      title: "Ongoing",
+      leads: onGoing,
+    },
+    {
+      title: "Intent",
+      leads: intent,
+    },
+    {
+      title: "Purchased",
+      leads: purchased,
+    },
+  ];
+
+  return (
+    <div className="h-full overflow-hidden">
+      <Title>Sales Funnel Management</Title>
+
+      <div className="flex justify-between p-5">
+        {data.map((item, index) => (
+          <div key={index} className="w-[19%]">
+            <h2 className="text-white bg-[#006D77] px-4 py-3 rounded-lg">
+              <p className="text-bsae font-bold ">{item.title}</p>
+              <p className="text-sm text-slate-200">
+                {
+                  // Display the number of leads in the category
+                  // check if the leads empty or not
+                  item.leads[0] &&
+                  item.leads[0].name === "" &&
+                  item.leads[0].email === "" &&
+                  item.leads[0].phone === ""
+                    ? 0
+                    : item.leads.length
+                }{" "}
+                Leads
+              </p>
+            </h2>
+
+            <ul className="mt-5 flex flex-col gap-3 h-[38%] overflow-auto p-1">
+              {item.leads.map((lead, index) => (
+                <li
+                  key={index}
+                  className="min-h-[150px] max-h-[150px] app-shadow rounded-2xl p-3"
+                >
+                  <div>
+                    <h3 className="text-base font-semibold overflow-auto pb-2">
+                      {lead.name}
+                    </h3>
+                    <p className="text-sm overflow-auto pb-2">{lead.email}</p>
+                    <p className="text-sm overflow-auto pb-2">{lead.phone}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
